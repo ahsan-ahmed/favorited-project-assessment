53a7c74115ddbd521e05e77d627d3e235c1b6d23
ahsan-ahmed <ahsanahmed0124@gmail.com>
2025-01-06 15:43:42 +0500

    Fixed the error of css loader not installed.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx', // Use .tsx as the entry point
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(ts|tsx)$/, // Match .ts and .tsx files
+                test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use: 'ts-loader',
             },
@@ -21,7 +21,7 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'], // Add .ts and .tsx extensions
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     plugins: [
         new HtmlWebpackPlugin({
