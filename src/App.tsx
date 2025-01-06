562d5e3b3fc8cc9fcc7a7ad720e1737599b657e0
ahsan-ahmed <ahsanahmed0124@gmail.com>
2025-01-06 15:34:26 +0500

    Added webpack conf., added typescript, initialized git, added project structure and entry point

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const App: React.FC = () => {
+  return <Button variant="contained">Click Me</Button>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx', // Use .tsx as the entry point
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/, // Match .ts and .tsx files
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'], // Add .ts and .tsx extensions
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        hot: true,
+        port: 3000,
+    },
+};
